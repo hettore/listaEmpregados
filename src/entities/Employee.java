e955116815699b0df00b71ad06e95aca4d3ba879
Hettore Eduardo <duh.barretoo@gmail.com>
2022-08-23 15:31:19 -0300

    Ajustando as casas decimais do salário no to string.

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -45,7 +45,7 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return id + ", " + name + ", " + salary;
+		return id + ", " + name + ", " + String.format("%.2f", salary);
 	}
 
 }
